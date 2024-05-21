bf8607b7ac674e6f922870f637ac8177beefc398
BatoulAlaghwane2000 <alagwanebtoul@gmail.com>
2024-05-21 00:53:46 -0800

    add function loginadmin , create middleware for admin

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\AccessoryPartController;
 use App\Http\Controllers\SparePartController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\OrdersSparePartController;
+use GuzzleHttp\Middleware;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -67,14 +69,16 @@ Route::post('addOrderSparePart', [OrdersSparePartController::class, 'addOrderSpa
 
 
 #region admin
-Route::middleware(['auth:api', 'admin:1'])->group(function(){
+
+     Route::post('/loginAdmin', [AuthController::class, 'loginAdmin']);
         Route::prefix('admin')->group(function () {
-        Route::post('/login', [AuthController::class, 'loginAdmin']);
-        Route::post('/storeAccessoryPart', [AccessoryPartController::class, 'storeAccessoryPart']);
-        Route::post('/storeCarForReservation', [CarController::class, 'storeCarForReservation']);
-        Route::post('/storeCarForBuying', [CarController::class, 'storeCarForBuying']);
-        Route::post('/storeSparePart', [SparePartController::class, 'storeSparePart']);
-    });
-});
+            Route::group(['middleware'=>['admin']],function(){
+                Route::post('/storeAccessoryPart', [AccessoryPartController::class, 'storeAccessoryPart']);
+                Route::post('/storeCarForReservation', [CarController::class, 'storeCarForReservation']);
+                Route::post('/storeCarForBuying', [CarController::class, 'storeCarForBuying']);
+                Route::post('/storeSparePart', [SparePartController::class, 'storeSparePart']);
+            });
+            });
+
 
 #end region admin
